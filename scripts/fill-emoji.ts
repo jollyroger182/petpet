9fbb75bde52f7751f3ca49f3594aba59a21d53f5
jollyroger182 <hello@jollyy.dev>
2026-07-16 21:27:19 -0400

    single creator

--- scripts/fill-emoji.ts
+++ scripts/fill-emoji.ts
@@ -1,13 +1,14 @@
 import DB from '../src/database'
 
+const creator = prompt('creator:')
+if (!creator) process.exit()
+
 while (true) {
   const user = prompt('user:')
   if (!user) break
   while (true) {
     const name = prompt('name:')
     if (!name) break
-    const creator = prompt('creator:')
-    if (!creator) break
 
     await DB.addEmoji({ creator, name, target_user: user })
   }
